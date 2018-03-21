6aa446b557d26f43e7069160716bf980bb8bb0cd
Dumenil <antoine.dumenil@gmail.com>
2018-03-21 15:43:36 +0100

    FakeResult

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
+
+
+
+export interface Result{
+  title: string;
+  author:string;
+  img:string;
+}
+
+const fakeResults: Result[] = [
+  { title: 'neoingroednog 1', author: ' John 1', img:'assets/imgs/pers1.png'},
+  { title: 'neoingroednog 2', author: ' John 2', img: 'assets/imgs/pers2.png' },
+  { title: 'neoingroednog 3', author: ' John 3', img: 'assets/imgs/pers3.jpg' }
+];
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  results: Result[];
   constructor(public navCtrl: NavController) {
-
+    this.results = fakeResults;
   }
 
 }
